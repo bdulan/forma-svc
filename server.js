13c1bfab2add8681f2a522a61551e7c7cc5a388e
Brett Dulan <bdulan715@gmail.com>
2021-03-09 16:02:40 -0800

    Update all docs

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var cors = require('cors')
+var app = express();
+var path = require('path');
+
+app.use(cors())
+
+// viewed at http://localhost:3001
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3002, function () {
+    console.log('CORS-enabled web server listening on port 3002')
+  })
